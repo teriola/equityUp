3307b35ae4a048cbd850004adb6adcb3919334f1
Kristiyan <bkristiqn7@gmail.com>
2024-03-05 23:24:27 +0200

    Add Register form boilerplate

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,15 +1,26 @@
+import RegisterStyles from './Register.module.css';
+
 function Register() {
     return (
         <>
-            <form>
-                <label htmlFor="email">Email</label>
-                <input id="email" type="email" />
-
-                <label htmlFor="username">Username</label>
-                <input id="username" type="text" />
-
-                <label htmlFor="password">Password</label>
-                <input type="password" />
+            <form className={RegisterStyles.form}>
+                <div className={RegisterStyles.control}>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" required />
+                </div>
+                <div className={RegisterStyles.control}>
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username" required />
+                </div>
+                <div className={RegisterStyles.control}>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" required />
+                </div>
+                <div className={RegisterStyles.control}>
+                    <label htmlFor="rePassword">Re-Password</label>
+                    <input type="password" id="rePassword" required />
+                </div>
+                <button>Register</button>
             </form>
         </>
     );
